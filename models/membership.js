b7c283825599ab5031a9f0abd1e8acbdb282616f
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-07-09 15:13:25 +0530

    implemented logout

--- models/membership.js
+++ models/membership.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const memberSchema = mongoose.Schema({
     mobile: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     name: {
         type: String,
